15f900c66f7a4eb719833dfcd05281068f28882f
Jake <github@jakewendt.com>
2021-03-25 15:22:29 -0700

    Added logger setup so actually outputs something

--- consolidate.py
+++ consolidate.py
@@ -16,6 +16,24 @@ import logging
 # Configure logger
 logger = logging.getLogger('root')
 
+logger.setLevel(logging.DEBUG)
+
+# create console handler and set level to debug
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+
+# create formatter
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+# add formatter to ch
+ch.setFormatter(formatter)
+
+# add ch to logger
+logger.addHandler(ch)
+
+
+
+
 def read_bins(fastq_file):
     infile = HTSeq.FastqReader(fastq_file)
     read_num = 0
